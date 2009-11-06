0af4a81e08b98d11a82f7d5e41a519001d975122
John Van Enk <john@carbon.(none)>
2009-11-06 10:30:15 -0500

    Finished up MPI.Format

--- src/GCrypt/MPI/Format.hs
+++ src/GCrypt/MPI/Format.hs
@@ -1,6 +1,9 @@
 module GCrypt.MPI.Format (
     mpiScan,
     mpiScanBS,
+    mpiPrint, -- unsafe
+    mpiAPrint,
+    mpiDump,
 ) where
 
 import GCrypt.Util
@@ -8,6 +11,7 @@ import GCrypt.Util
 import GPG.Error
 import GCrypt.Base
 import Foreign.Ptr
+import Foreign.C.Types
 import Foreign.Storable
 
 import Data.ByteString
@@ -36,3 +40,22 @@ mpiScanBS :: MPIFormat -> ByteString -> IO (Either GCry_Error (MPI,Int))
 mpiScanBS f b = do
     -- castPtr is doing Ptr Char -> Ptr ()
     unsafeUseAsCStringLen b $ \(s,l) -> mpiScan f (castPtr s) l
+
+mpiPrint :: MPIFormat -> Ptr () -> CSize -> CSizePtr -> MPI -> IO GCry_Error
+mpiPrint f b s sp m = gcry_mpi_print f (castPtr b) s sp m
+
+mpiAPrint :: MPIFormat -> MPI -> IO (Either GCry_Error (Ptr (),Int))
+mpiAPrint fmt mpi = do
+    (buf,len,ret) <- newWith2 f
+
+    case toIntEnum ret of
+        -- castPtr is doing Ptr CUChar -> Ptr ()
+        GPG_ERR_NO_ERROR -> return $ Right (castPtr buf, fromIntegral len)
+        _ -> return $ Left ret
+    where
+        f :: Ptr (Ptr CUChar) -> Ptr CSize -> IO GCry_Error
+        -- castPtr is doing Ptr CSize -> Ptr CSizePtR
+        f b s = gcry_mpi_aprint fmt b (CSizePtr $ castPtr s) mpi
+
+mpiDump :: MPI -> IO ()
+mpiDump m = gcry_mpi_dump m
