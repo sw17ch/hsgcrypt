96ee1c0eb402a9757dd60c7678696a438322e1ad
John Van Enk <john@carbon.(none)>
2009-11-06 00:07:57 -0500

    Added mpiScan and mpiScanBS to MPI.Format

--- src/GCrypt/MPI/Format.hs
+++ src/GCrypt/MPI/Format.hs
@@ -1,7 +1,38 @@
 module GCrypt.MPI.Format (
-    
+    mpiScan,
+    mpiScanBS,
 ) where
 
+import GCrypt.Util
+
+import GPG.Error
 import GCrypt.Base
+import Foreign.Ptr
+import Foreign.Storable
 
+import Data.ByteString
+import Data.ByteString.Unsafe
 
+-- Reads from buffer with specified format into a new MPI. Returns
+-- (new mpi, number bytes scanned from buffer).
+mpiScan :: MPIFormat -> Ptr () -> Int -> IO (Either GCry_Error (MPI,Int))
+mpiScan format buffer buflen = do
+    (mpi,nscanned,ret) <- newWith2 f
+
+    -- castPtr is doing Ptr CSizePtr -> Ptr CSize
+    n <- peek $ castPtr $ unCSizePtr nscanned
+    case toIntEnum ret of
+         GPG_ERR_NO_ERROR -> return $ Right (mpi, n)
+         _ -> return $ Left ret
+    where
+        f :: Ptr MPI -> Ptr CSizePtr -> IO GCry_Error
+        f m n = gcry_mpi_scan (MPIPtr m)
+                              format
+                              buffer
+                              (fromIntegral buflen)
+                              (CSizePtr n)
+
+mpiScanBS :: MPIFormat -> ByteString -> IO (Either GCry_Error (MPI,Int))
+mpiScanBS f b = do
+    -- castPtr is doing Ptr Char -> Ptr ()
+    unsafeUseAsCStringLen b $ \(s,l) -> mpiScan f (castPtr s) l
