187c0c74dcae11c9c77375388347651b22fe2b9e
John Van Enk <john@carbon.(none)>
2009-11-06 12:00:50 -0500

    Working through MPI.Bits

--- src/GCrypt/MPI/Bits.hs
+++ src/GCrypt/MPI/Bits.hs
@@ -1,1 +1,19 @@
-module GCrypt.MPI.Bits where
+module GCrypt.MPI.Bits (
+    getNBits,
+    testBit,
+    setBit,
+    
+    module Foreign.C.Types,
+) where
+
+import GCrypt.Base
+import Foreign.C.Types
+
+getNBits :: MPI -> IO CUInt
+getNBits = gcry_mpi_get_nbits
+
+testBit :: MPI -> CUInt -> IO CInt
+testBit = gcry_mpi_test_bit
+
+setBit :: MPI -> CUInt -> IO ()
+setBit = gcry_mpi_set_bit
