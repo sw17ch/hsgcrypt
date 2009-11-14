6c0e954bf7ccd7f2d401ba53fc9e71ed5bec2f36
John Van Enk <vanenkj@gmail.com>
2009-11-13 23:53:17 -0500

    Finished GCrypt.MPI.Bits

--- src/GCrypt/MPI/Bits.hs
+++ src/GCrypt/MPI/Bits.hs
@@ -2,6 +2,10 @@ module GCrypt.MPI.Bits (
     getNBits,
     testBit,
     setBit,
+    clearBit,
+    setHighBit,
+    clearHighBit,
+    rshift,
     
     module Foreign.C.Types,
 ) where
@@ -17,3 +21,21 @@ testBit = gcry_mpi_test_bit
 
 setBit :: MPI -> CUInt -> IO ()
 setBit = gcry_mpi_set_bit
+
+clearBit :: MPI -> CUInt -> IO ()
+clearBit = gcry_mpi_clear_bit
+
+setHighBit :: MPI -> CUInt -> IO ()
+setHighBit = gcry_mpi_set_highbit
+
+clearHighBit :: MPI -> CUInt -> IO ()
+clearHighBit = gcry_mpi_clear_highbit
+
+rshift :: MPI -> MPI -> CUInt -> IO ()
+rshift = gcry_mpi_rshift
+
+{- This wasn't defined in older versions
+ - for some reason...
+lshift :: MPI -> CUInt -> IO ()
+lshift = gcry_mpi_lshift
+-}
