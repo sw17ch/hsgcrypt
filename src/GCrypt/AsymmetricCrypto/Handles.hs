de40bc1c1f51e206d186c125acc572daba48f573
John Van Enk <vanenkj@gmail.com>
2009-11-24 18:28:53 -0500

    Finished AsymmetricCrypto.Handles

--- src/GCrypt/AsymmetricCrypto/Handles.hs
+++ src/GCrypt/AsymmetricCrypto/Handles.hs
@@ -1,1 +1,24 @@
-module GCrypt.AsymmetricCrypto.Handles where
+module GCrypt.AsymmetricCrypto.Handles (
+    ACHandle,
+    acOpen,
+    acClose,
+) where
+
+import Foreign.Ptr
+
+import GCrypt.Base
+import GCrypt.Util
+import GPG.Error
+
+checkData :: GCry_Error -> Bool
+checkData e = (toIntEnum e) == GPG_ERR_NO_ERROR
+
+acOpen :: GCry_AC_ID -> ACFlags -> IO (Either GCry_Error ACHandle)
+acOpen alg fl = do
+    newWithChecked f checkData
+    where
+        f :: Ptr ACHandle -> IO GCry_Error
+        f p = gcry_ac_open (ACHandlePtr p) alg fl
+
+acClose :: ACHandle -> IO ()
+acClose = gcry_ac_close
