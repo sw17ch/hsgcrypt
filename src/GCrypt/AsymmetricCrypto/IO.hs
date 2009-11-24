0eff17933448b11965d286a05a82d0a0831be123
John Van Enk <john@carbon.(none)>
2009-11-24 13:05:53 -0500

    Finished inital pass of AsymmetricCrypto.IO. This needs revisiting.

--- src/GCrypt/AsymmetricCrypto/IO.hs
+++ src/GCrypt/AsymmetricCrypto/IO.hs
@@ -1,1 +1,17 @@
-module GCrypt.AsymmetricCrypto.IO where
+module GCrypt.AsymmetricCrypto.IO (
+    initReadableString,
+    initWritableString,
+    ACIO,
+) where
+
+import Foreign.Ptr
+import Foreign.C.Types
+
+import GCrypt.Base
+
+initReadableString :: ACIO -> Ptr CUChar -> CULong -> IO ()
+initReadableString = gcry_ac_io_init_readable_string
+
+initWritableString :: ACIO -> Ptr (Ptr CUChar) -> Ptr CULong -> IO ()
+initWritableString = gcry_ac_io_init_writable_string
+
