065729cc34e25938556cdb5642124f4b89cd8252
John Van Enk <vanenkj@gmail.com>
2009-11-28 12:54:49 -0500

    Made it run, but not really, segfaults in encode.

--- examples/encode.hs
+++ examples/encode.hs
@@ -2,10 +2,11 @@ module Main where
 
 import Data.ByteString.Char8
 
+import GCrypt
 import GCrypt.AsymmetricCrypto.Crypto
 
 main :: IO ()
-main = do
+main = withGCrypt $ do
     e <- dataEMEEncode (OptionsEME 64) str
     print e
     where
