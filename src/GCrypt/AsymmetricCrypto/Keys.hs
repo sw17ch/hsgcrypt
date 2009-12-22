04419b529dea6147d1d9985919b1958cd8c8383b
John Van Enk <john@dorner-john-linux.(none)>
2009-12-22 17:39:02 -0500

    added keyInit and keyPairGenerate

--- src/GCrypt/AsymmetricCrypto/Keys.hs
+++ src/GCrypt/AsymmetricCrypto/Keys.hs
@@ -1,1 +1,44 @@
-module GCrypt.AsymmetricCrypto.Keys where
+{-# LANGUAGE EmptyDataDecls #-}
+module GCrypt.AsymmetricCrypto.Keys (
+    ACKey,
+    ACKeyPair,
+    keyInit,
+    keyPairGenerate,
+) where
+
+import Foreign.Ptr
+import Foreign.C.Types
+
+import GCrypt.Base
+import GCrypt.Common
+import GCrypt.Util
+
+
+{- TODO: Implement this so keyPairGenerate has more flexibility.
+typedef struct gcry_ac_key_spec_rsa
+{
+  gcry_mpi_t e;                 /* E to use.  */
+} gcry_ac_key_spec_rsa_t;
+-}
+
+keyInit :: ACHandle -> ACKeyType -> ACData -> IO (Either GCry_Error ACKey)
+keyInit h t d = do
+    newWithChecked f checkData
+    where
+        f key = gcry_ac_key_init (ACKeyPtr key) h t d
+
+keyPairGenerate :: ACHandle
+                -> CUInt
+                -- -> Maybe ACKeySpecRSA -- Taken out until supported
+                -> IO (Either GCry_Error ACKeyPair)
+keyPairGenerate h n {- s -} = do
+    newWithChecked f checkData
+    where
+        {-
+        s' = case s of
+                  Nothing -> ACKeySpecRSAPtr nullPtr
+                  Just _ -> error "keyPairGenerate currently does not support key specs"
+        -}
+        f kpair = gcry_ac_key_pair_generate h n (ACKeySpecRSAPtr nullPtr)
+            (ACKeyPairPtr kpair)
+            (MPIPtrPtr nullPtr)
