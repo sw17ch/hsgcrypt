b1579b91e55009087f5eda3484e2a0c8628bdca9
John Van Enk <john@dorner-john-linux.(none)>
2009-12-22 13:59:31 -0500

    Changed some formatting and reduced scope on prt2AP.

--- src/GCrypt/AsymmetricCrypto/Crypto.hs
+++ src/GCrypt/AsymmetricCrypto/Crypto.hs
@@ -19,7 +19,7 @@ import GCrypt.Generalities.Error.Strings
 import GPG.Error
 
 import Control.Monad
-import Data.ByteString hiding (putStrLn)
+import Data.ByteString hiding (putStrLn, concat)
 import Data.ByteString.Unsafe
 
 import Foreign.Ptr
@@ -30,58 +30,62 @@ import Foreign.Storable
 
 -- |Returns either an error or the encrypted data
 -- inside an ACData.
-dataEncrypt :: ACHandle -- | handle
-            -> ACFlags  -- | flags
-            -> ACKey    -- | key
-            -> MPI      -- | data_plain
+dataEncrypt :: ACHandle -- ^ handle
+            -> ACFlags  -- ^ flags
+            -> ACKey    -- ^ key
+            -> MPI      -- ^ data_plain
             -> IO (Either GCry_Error ACData)
 dataEncrypt h f k m = do
     newWithChecked fn checkData
     where
         fn d = gcry_ac_data_encrypt h f k m (ACDataPtr d)
 
-dataDecrypt :: ACHandle -- | handle
-            -> ACFlags  -- | flags
-            -> ACKey    -- | key
-            -> ACData   -- | data_encrypted
+dataDecrypt :: ACHandle -- ^ handle
+            -> ACFlags  -- ^ flags
+            -> ACKey    -- ^ key
+            -> ACData   -- ^ data_encrypted
             -> IO (Either GCry_Error MPI)
 dataDecrypt h f k d = do
     newWithChecked fn checkData
     where
         fn m = gcry_ac_data_decrypt h f k (MPIPtr m) d
 
-dataSign :: ACHandle -- | handle
-         -> ACKey    -- | key
-         -> MPI      -- | data_plain
+dataSign :: ACHandle -- ^ handle
+         -> ACKey    -- ^ key
+         -> MPI      -- ^ data_plain
          -> IO (Either GCry_Error ACData)
 dataSign h k m = do
     newWithChecked fn checkData
     where
         fn d = gcry_ac_data_sign h k m (ACDataPtr d)
 
-dataVerify :: ACHandle -- | handle
-           -> ACKey    -- | key
-           -> MPI      -- | data_plain
-           -> ACData   -- | data_signature
-           -> IO Bool  -- | True when the signature is verified.
+dataVerify :: ACHandle -- ^ handle
+           -> ACKey    -- ^ key
+           -> MPI      -- ^ data_plain
+           -> ACData   -- ^ data_signature
+           -> IO Bool  -- ^ True when the signature is verified.
 dataVerify h k m d = do
     ret <- gcry_ac_data_verify h k m d
     return $ (toIntEnum ret) == GPG_ERR_NO_ERROR
 
--- Cast a (Ptr ACIO) to an ACIOPtr
-ptr2AP :: Ptr ACIO -> ACIOPtr
-ptr2AP = ACIOPtr . castPtr
-
-dataEMEEncode :: OptionsEME -> ByteString -> IO (Either String ByteString)
+dataEMEEncode :: OptionsEME -- ^ EME options
+              -> ByteString -- ^ Data to encode
+              -> IO (Either String ByteString)
 dataEMEEncode o@(OptionsEME sze) s | sze < 88 = return $ Left segfaultWarning
                                    | otherwise = genEncode o s AC_EME_PKCS_V1_5
 
-dataEMSAEncode :: OptionsEMSA -> ByteString -> IO (Either String ByteString)
+dataEMSAEncode :: OptionsEMSA
+               -> ByteString
+               -> IO (Either String ByteString)
 dataEMSAEncode o s = genEncode o s AC_EMSA_PKCS_V1_5
 
 -- Generic encoding function since the interface is just about the same
 -- for dataEMEEncode and dataEMSAEncode.
-genEncode :: (Storable a) => a -> ByteString -> GCry_EncMethod -> IO (Either String ByteString)
+genEncode :: (Storable a)
+          => a -- Options data type
+          -> ByteString -- Data to encode
+          -> GCry_EncMethod -- Encode method
+          -> IO (Either String ByteString)
 genEncode o s m = do
     r_io <- initReadableByteString s
     w_io <- mkACIO
@@ -99,6 +103,8 @@ genEncode o s m = do
     case ret of 
         0 -> (s_l_2_bs ns nl) >>= return . Right
         _ -> strerror ret >>= return . Left
+    where
+        ptr2AP = ACIOPtr . castPtr
 
 s_l_2_bs :: ForeignPtr (Ptr CUChar) -> ForeignPtr CULong -> IO ByteString
 s_l_2_bs ns nl =
@@ -110,6 +116,6 @@ s_l_2_bs ns nl =
                                    (gcry_free $ castPtr str)
 
 segfaultWarning :: String
-segfaultWarning = unlines [
-    "Error: The size you passed to dataEMEEncode in OptionsEME is too short.",
+segfaultWarning = concat [
+    "Error: The size you passed to dataEMEEncode in OptionsEME is too short. ",
     "libgcrypt would segfault if I allowed execution. I'm quitting now." ]
